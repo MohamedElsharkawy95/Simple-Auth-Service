c10a383953ad6fb454d156c63db3162f28f67fa5
mohamed.elsharkawy <mohamed.elsharkawy@robustastudio.com>
2025-01-11 17:48:27 -0800

    integrate serilog

--- obj/Debug/net8.0/AuthAPI.AssemblyInfo.cs
+++ obj/Debug/net8.0/AuthAPI.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AuthAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d74f32cabb88744a3f4dbb68bcead74b4ccc9513")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4a1fa7ac0242b225f16cfc7397b9e2560fa0f103")]
 [assembly: System.Reflection.AssemblyProductAttribute("AuthAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AuthAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
